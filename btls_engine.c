044227a5532579b3be00f54392a28aca08c34abc
blackbearman <blackbearman@rambler.ru>
2014-08-07 15:18:59 +0300

    Add another files

--- btls_engine.c
+++ btls_engine.c
@@ -21,6 +21,14 @@
 #include "btls_err.h"
 #include "btls_engine.h"
 
+#define SSL_CIPHER_SET_DYNAMIC
+
+#ifdef SSL_CIPHER_SET_DYNAMIC
+#include <openssl/ssl.h>
+#include "ssl_spec.h"
+
+#endif
+
 static const char *engine_btls_id = "btls_e";
 static const char *engine_btls_name = "BTLS_E Engine [belt + bign support]";
 
@@ -51,7 +59,7 @@ int meth_nids[] = {NID_undef, NID_undef};
 		OBJ_create(OID_##alg, BUF_strdup(SN_##alg) , BUF_strdup(LN_##alg)) : tmpnid;\
 	if (var == NID_undef) { goto err;}
 
-static int register_NIDs() 
+static int register_NIDs()
 {
 	int tmpnid;
 
@@ -66,16 +74,16 @@ static int register_NIDs()
 	REGISTER_NID(bign_pubkey_nid, bign_pubkey)
 	REGISTER_NID(bign_with_hbelt_nid, bign_with_hbelt)
 
-	if (!OBJ_add_sigid(bign_with_hbelt_nid, belt_hash_nid, bign_pubkey_nid))  
+	if (!OBJ_add_sigid(bign_with_hbelt_nid, belt_hash_nid, bign_pubkey_nid))
 		goto err;
 
 	belt_digest_nids[0] = belt_hash_nid;
-	belt_digest_nids[1] = belt_mac_nid; 
+	belt_digest_nids[1] = belt_mac_nid;
 
-	belt_cipher_nids[0] = belt_stream_nid; 
-	belt_cipher_nids[1] = belt_ctr_nid; 
-	belt_cipher_nids[2] = belt_cfb_nid; 
-	belt_cipher_nids[3] = belt_dwp_nid; 
+	belt_cipher_nids[0] = belt_stream_nid;
+	belt_cipher_nids[1] = belt_ctr_nid;
+	belt_cipher_nids[2] = belt_cfb_nid;
+	belt_cipher_nids[3] = belt_dwp_nid;
 
 	meth_nids[0] = belt_mac_nid;
 	meth_nids[1] = bign_pubkey_nid;
@@ -108,15 +116,130 @@ EVP_PKEY_METHOD *bign_pmeth = NULL;
 EVP_PKEY_ASN1_METHOD *belt_mac_ameth = NULL;
 EVP_PKEY_ASN1_METHOD *bign_ameth = NULL;
 
+
+#ifdef SSL_CIPHER_SET_DYNAMIC
+/*
+*******************************************************************************
+Структуры SSL_CIPHER
+*******************************************************************************
+*/
+
+static ENC_SPEC enc_spec_BELT_CTR = { { 0 }, "BELT-CTR", NULL, &belt_ctr, 0 };
+
+
+static MAC_SPEC mac_spec_GOST94 = { { 0 }, "BELT-MAC", NULL, &belt_mac, 0 };
+
+
+AUTH_SPEC auth_spec_GOST;
+
+
+static KEYEXCH_SPEC kex_spec_CPGOST94 = { { 0 }, "GOST", NULL, 1,
+	CPGOST_prepare_client_key_exchange, //prepare_client_key_exchange should be implemented
+	NULL,
+	NULL,
+	CPGOST_parse_client_key_exchange,  //parse_client_key_exchange should be implemented 1st
+	CPGOST94_cert_cipher_compat_p
+};
+
+static PRF_SPEC prf_spec_GOST = {
+	{ 0 },
+	"GOST PRF",
+	NULL,
+	gost_prf_func,
+	0
+};
+
+#define TLS1_TXT_DH_WITH_BELT_128_SHA256 "GOST94-GOST89-GOST89"
+#define TLS1_CK_DH_WITH_BELT_128_SHA256 0x3000080
+/* Bits for algorithm_mkey (key exchange algorithm) */
+#define SSL_kBELT       0x00000800L /* BELT key exchange */
+/* Bits for algorithm_auth (server authentication) */
+#define SSL_aBELT 	    0x00000800L /* BELT signature auth */
+/* Bits for algorithm_enc (symmetric encryption) */
+#define SSL_eBELTCNT		0x00008000L
+/* Bits for algorithm_mac (symmetric authentication) */
+#define SSL_BELTMAC     0x00000080L
+/* Bits for algorithm_ssl (protocol version) */
+#define SSL_TLSV1_2		0x00000004L
+/* Bits for algorithm2 (handshake digests and other extra flags) */
+
+#define SSL_HANDSHAKE_MAC_BELT 0x200
+
+#define TLS1_PRF_DGST_SHIFT 10
+#define TLS1_PRF_BELT (SSL_HANDSHAKE_MAC_BELT << TLS1_PRF_DGST_SHIFT)
+#define TLS1_STREAM_MAC 0x04
+
+
+static SSL_CIPHER tls_gost341094_with_gost28147_ofb_gost28147 =
+	{
+    1,
+    TLS1_TXT_DH_WITH_BELT_128_SHA256,
+    TLS1_CK_DH_WITH_BELT_128_SHA256,
+    SSL_kBELT,
+    SSL_aBELT,
+    SSL_eBELTCNT,
+    SSL_BELTMAC,
+    SSL_TLSV1_2,
+    SSL_NOT_EXP|SSL_HIGH,
+    SSL_HANDSHAKE_MAC_BELT|TLS1_PRF_BELT|TLS1_STREAM_MAC,
+    256,
+    256
+};
+static SSL_CIPHER_SPEC tls_gost341094_with_gost28147_ofb_gost28147_spec =
+{
+	1,
+	//"TLS_GOST341094_WITH_GOST28147_OFB_GOST28147",
+	"GOST94-GOST89-GOST89",
+	0x03000080,
+	0x02000000L,
+	&kex_spec_CPGOST94,
+	&auth_spec_GOST,
+	&prf_spec_GOST,
+	&enc_spec_BELT_CTR,
+	&mac_spec_GOST94,
+	{NID_undef, NID_undef}, //NID_gost
+	SSL_NOT_EXP|SSL_HIGH,
+	0,
+	256,
+	256,
+	SSL_ALL_STRENGTHS
+};
+
+static int init_gost_ssl_cipher_spec(void)
+{
+
+	if (!SSL_register_enc_spec(&enc_spec_BELT_CTR)) goto end;
+	if (!SSL_register_mac_spec(&mac_spec_GOST94)) goto end;
+	if (!SSL_register_prf_spec(&prf_spec_GOST)) goto end;
+	kex_spec_CPGOST94.mask.mask = 0x00000004L; /* SSL_kDHd */
+	/* clone and modify auth_spec_DSS */
+//	auth_spec_GOST = *SSL_auth_spec_DSS();
+	auth_spec_GOST.md_nid[0] = belt_hash.type;//digest_gost.type;
+	auth_spec_GOST.cert_verify_dgst_len = 32;//digest_gost.md_size;
+	auth_spec_GOST.get_and_verify_server_params = GOST_verify_server_params;
+	auth_spec_GOST.authenticate_server_params = GOST_sign_server_params;
+/*	if (!SSL_register_auth_spec(&auth_spec_GOST)) goto end */
+
+	tls_gost341094_with_gost28147_ofb_gost28147_spec.finished_md_nid[0] = belt_ctr_nid;//NID_digest_GOST;
+	if (!tls1_register_cipher_suite(&tls_gost341094_with_gost28147_ofb_gost28147_spec)) goto end;
+		return 1;
+end:
+		return 0;
+}
+
+
+
+
+#endif // SSL_CIPHER_SET_DYNAMIC
 /*
 *******************************************************************************
 Callbacks
 *******************************************************************************
 */
 
-static int belt_ciphers(ENGINE * e, const EVP_CIPHER ** cipher, const int ** nids, int nid) 
+static int belt_ciphers(ENGINE * e, const EVP_CIPHER ** cipher, const int ** nids, int nid)
 {
-	if (cipher == NULL) 
+	if (cipher == NULL)
 	{
 		*nids = belt_cipher_nids;
 		return sizeof(belt_cipher_nids) / sizeof(belt_cipher_nids[0]);
@@ -136,14 +259,14 @@ static int belt_ciphers(ENGINE * e, const EVP_CIPHER ** cipher, const int ** nid
 	return 1;
 }
 
-static int belt_digest(ENGINE * engine, const EVP_MD ** evp_md, const int ** nids, int nid) 
+static int belt_digest(ENGINE * engine, const EVP_MD ** evp_md, const int ** nids, int nid)
 {
-	if (evp_md == NULL) 
+	if (evp_md == NULL)
 	{
 		*nids = belt_digest_nids;
 		return sizeof(belt_digest_nids) / sizeof(belt_digest_nids[0]);
 	}
-	
+
 	if (nid == belt_hash_nid)
 			*evp_md = &belt_hash;
 	else if (nid == belt_mac_nid)
@@ -154,9 +277,9 @@ static int belt_digest(ENGINE * engine, const EVP_MD ** evp_md, const int ** nid
 	return 1;
 }
 
-static int belt_pkey_meths(ENGINE *e, EVP_PKEY_METHOD **pmeth, const int **nids, int nid) 
+static int belt_pkey_meths(ENGINE *e, EVP_PKEY_METHOD **pmeth, const int **nids, int nid)
 {
-	if (!pmeth) 
+	if (!pmeth)
 	{
 		*nids = meth_nids;
 		return sizeof(meth_nids) / sizeof(meth_nids[0]);
@@ -173,9 +296,9 @@ static int belt_pkey_meths(ENGINE *e, EVP_PKEY_METHOD **pmeth, const int **nids,
 	return 1;
 }
 
-static int belt_pkey_asn1_meths(ENGINE *e, EVP_PKEY_ASN1_METHOD **ameth, const int **nids, int nid) 
+static int belt_pkey_asn1_meths(ENGINE *e, EVP_PKEY_ASN1_METHOD **ameth, const int **nids, int nid)
 {
-	if (!ameth) 
+	if (!ameth)
 	{
 		*nids = meth_nids;
 		return sizeof(meth_nids) / sizeof(meth_nids[0]);
@@ -192,29 +315,29 @@ static int belt_pkey_asn1_meths(ENGINE *e, EVP_PKEY_ASN1_METHOD **ameth, const i
 	return 1;
 }
 
-static int add() 
+static int add()
 {
-	if (!EVP_add_digest(&belt_hash)) 
+	if (!EVP_add_digest(&belt_hash))
 	{
 		return 0;
 	}
-	if (!EVP_add_digest(&belt_mac)) 
+	if (!EVP_add_digest(&belt_mac))
 	{
 		return 0;
 	}
-	if (!EVP_add_cipher(&belt_stream)) 
+	if (!EVP_add_cipher(&belt_stream))
 	{
 		return 0;
 	}
-	if (!EVP_add_cipher(&belt_cfb)) 
+	if (!EVP_add_cipher(&belt_cfb))
 	{
 		return 0;
 	}
-	if (!EVP_add_cipher(&belt_ctr)) 
+	if (!EVP_add_cipher(&belt_ctr))
 	{
 		return 0;
 	}
-	if (!EVP_add_cipher(&belt_dwp)) 
+	if (!EVP_add_cipher(&belt_dwp))
 	{
 		return 0;
 	}
@@ -229,17 +352,17 @@ int btls_control_func(ENGINE *e,int cmd,long i, void *p, void (*f)(void))
 }
 
 static int btls_engine_init(ENGINE *e)
-{ 
+{
 	return 1;
 }
 
 static int btls_engine_finish(ENGINE *e)
-{ 
+{
 	return 1;
 }
 
 static int btls_engine_destroy(ENGINE *e)
-{ 
+{
 	belt_mac_pmeth = NULL;
 	bign_pmeth = NULL;
 	belt_mac_ameth = NULL;
@@ -248,9 +371,9 @@ static int btls_engine_destroy(ENGINE *e)
 }
 
 
-static int bind_btls(ENGINE * e, const char *id) 
+static int bind_btls(ENGINE * e, const char *id)
 {
-	if (id && strcmp(id, engine_btls_id)) 
+	if (id && strcmp(id, engine_btls_id))
 		return 0;
 
 	if (bign_ameth)
@@ -258,7 +381,7 @@ static int bind_btls(ENGINE * e, const char *id)
 		printf("btls-engine already loaded\n");
 		return 0;
 	}
-	
+
 	if (!register_NIDs()) {
 		printf("nids register error\n");
 		return 0;
@@ -285,69 +408,69 @@ static int bind_btls(ENGINE * e, const char *id)
 
 	belt_hash.pkey_type = bign_pubkey_nid;
 
-	if (!ENGINE_set_id(e, engine_btls_id)) 
+	if (!ENGINE_set_id(e, engine_btls_id))
 	{
 		printf("ENGINE_set_id failed\n");
 		return 0;
 	}
-	if (!ENGINE_set_name(e, engine_btls_name)) 
+	if (!ENGINE_set_name(e, engine_btls_name))
 	{
 		printf("ENGINE_set_name failed\n");
 		return 0;
 	}
-	if (!ENGINE_set_digests(e, belt_digest)) 
+	if (!ENGINE_set_digests(e, belt_digest))
 	{
 		printf("ENGINE_set_digests failed\n");
 		return 0;
 	}
-	if (!ENGINE_set_ciphers(e, belt_ciphers)) 
+	if (!ENGINE_set_ciphers(e, belt_ciphers))
 	{
 		printf("ENGINE_set_ciphers failed\n");
 		return 0;
 	}
-	if (!ENGINE_set_pkey_meths(e, belt_pkey_meths)) 
+	if (!ENGINE_set_pkey_meths(e, belt_pkey_meths))
 	{
 		printf("ENGINE_set_pkey_meths failed\n");
 		return 0;
 	}
-	if (!ENGINE_set_pkey_asn1_meths(e, belt_pkey_asn1_meths)) 
+	if (!ENGINE_set_pkey_asn1_meths(e, belt_pkey_asn1_meths))
 	{
 		printf("ENGINE_set_pkey_asn1_meths failed\n");
 		return 0;
 	}
 
-	if (!register_ameth_belt(belt_mac.type, &belt_mac_ameth, SN_belt_mac, LN_belt_mac)) 
+	if (!register_ameth_belt(belt_mac.type, &belt_mac_ameth, SN_belt_mac, LN_belt_mac))
 	{
 		printf("register_ameth_belt for MAC failed\n");
 		return 0;
 	}
-	if (!register_pmeth_belt(belt_mac.type, &belt_mac_pmeth, 0)) 
+	if (!register_pmeth_belt(belt_mac.type, &belt_mac_pmeth, 0))
 	{
 		printf("register_pmeth_belt for MAC failed\n");
 		return 0;
 	}
-	if (!register_ameth_bign(bign_pubkey_nid, &bign_ameth, SN_bign_pubkey, LN_bign_pubkey)) 
+	if (!register_ameth_bign(bign_pubkey_nid, &bign_ameth, SN_bign_pubkey, LN_bign_pubkey))
 	{
 		printf("register_ameth_bign failed\n");
 		return 0;
 	}
-	if (!register_pmeth_bign(bign_pubkey_nid, &bign_pmeth, 0)) 
+	if (!register_pmeth_bign(bign_pubkey_nid, &bign_pmeth, 0))
 	{
 		printf("register_pmeth_bign failed\n");
 		return 0;
 	}
 
 	/* Control function and commands */
-	if (!ENGINE_set_cmd_defns(e, btls_cmds)) 
+	if (!ENGINE_set_cmd_defns(e, btls_cmds))
 	{
 		fprintf(stderr,"ENGINE_set_cmd_defns failed\n");
 		return 0;
-	}	
-	if (!ENGINE_set_ctrl_function(e, btls_control_func)) 
+	}
+	if (!ENGINE_set_ctrl_function(e, btls_control_func))
 	{
 		fprintf(stderr,"ENGINE_set_ctrl_func failed\n");
 		return 0;
-	}	
+	}
 	if ( ! ENGINE_set_destroy_function(e, btls_engine_destroy)
 		|| ! ENGINE_set_init_function(e, btls_engine_init)
 		|| ! ENGINE_set_finish_function(e, btls_engine_finish))
@@ -356,12 +479,12 @@ static int bind_btls(ENGINE * e, const char *id)
 	}
 
 	/* Register and add algorithms */
-	if (!ENGINE_register_ciphers(e) || 
-		!ENGINE_register_digests(e) || 
+	if (!ENGINE_register_ciphers(e) ||
+		!ENGINE_register_digests(e) ||
 		!ENGINE_register_pkey_meths(e) ||
 		!EVP_add_digest(&belt_hash) ||
-		!EVP_add_digest(&belt_mac) || 
-		!EVP_add_cipher(&belt_stream) || 
+		!EVP_add_digest(&belt_mac) ||
+		!EVP_add_cipher(&belt_stream) ||
 		!EVP_add_cipher(&belt_cfb) ||
 		!EVP_add_cipher(&belt_ctr) ||
 		!EVP_add_cipher(&belt_dwp))
@@ -369,7 +492,9 @@ static int bind_btls(ENGINE * e, const char *id)
 		printf("ENGINE register / EVP_add failed\n");
 		return 0;
 	}
-
+#ifdef SSL_CIPHER_SET_DYNAMIC
+    if (! init_gost_ssl_cipher_spec()) return 0;
+#endif
 	ERR_load_BTLS_strings();
 	return 1;
 }
@@ -381,25 +506,25 @@ IMPLEMENT_DYNAMIC_CHECK_FN()
 
 #ifdef OPENSSL_NO_DYNAMIC_ENGINE
 static ENGINE *engine_btls(void)
-{	
+{
 	ENGINE *ret = ENGINE_new();
 	if (!ret)
 		return NULL;
-	if (!bind_btls(ret, engine_btls_id)) 
+	if (!bind_btls(ret, engine_btls_id))
 	{
 		ENGINE_free(ret);
 		return NULL;
 	}
 	return ret;
 }
-	
+
 void ENGINE_load_btls(void)
 {
 	ENGINE *toadd;
 	if (bign_ameth)
 		return;
 	toadd = engine_btls();
-	if (!toadd) 
+	if (!toadd)
 		return;
 	ENGINE_add(toadd);
 	ENGINE_free(toadd);
--- btls_engine.h
+++ btls_engine.h
@@ -18,12 +18,25 @@ extern "C" {
 
 #define ENGINE_NAME "btls_e"
 
-#undef OPENSSL_NO_DYNAMIC_ENGINE 
+#undef OPENSSL_NO_DYNAMIC_ENGINE
 
 #ifdef OPENSSL_NO_DYNAMIC_ENGINE
 void ENGINE_load_btls(void);
 #endif	/* OPENSSL_NO_DYNAMIC_ENGINE */
+#include "ssl_spec.h"
 
+/// TEMP functions
+int CPGOST_prepare_client_key_exchange (SSL *s, unsigned char *p);
+int CPGOST_parse_client_key_exchange (SSL *s, unsigned char *data, long msg_len);
+int CPGOST_cert_cipher_compat_p(X509 *cert,SSL_CIPHER_SPEC *c);
+int CPGOST94_cert_cipher_compat_p(X509 *cert,SSL_CIPHER_SPEC *c);
+int GOST_sign_server_params (SSL *s, unsigned char *param,
+									int param_len, unsigned char *buf);
+long GOST_verify_server_params (SSL *s, unsigned char *buf, long restlen);
+void gost_prf_func(SSL *s, unsigned char *label, int label_len,
+		const unsigned char *sec, int slen, unsigned char *out,
+		unsigned char *tmp, int olen);
+/// Temp functions
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
